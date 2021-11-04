21df889578bc16c1b74dd2202e179c634e166761
Vanessa D'mello <dmello.vanessa2001@gmail.com>
2021-11-04 17:44:08 +0530

    Add arrow function in App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,22 +2,33 @@ import React from "react";
 import Entry from "./Entry";
 import emojipedia from "../emoji";
 
-function createEntry(emojidetails) {
-	return (
-		<Entry
-			key={emojidetails.id}
-			emoji={emojidetails.emoji}
-			name={emojidetails.name}
-			meaning={emojidetails.meaning}
-		/>
-	);
-}
+// function createEntry(emojidetails) {
+// 	return (
+// 		<Entry
+// 			key={emojidetails.id}
+// 			emoji={emojidetails.emoji}
+// 			name={emojidetails.name}
+// 			meaning={emojidetails.meaning}
+// 		/>
+// 	);
+// }
 
 function App() {
 	return (
 		<div>
-			<h1><span>emojipedia</span></h1>
-			<div id="dictionary">{emojipedia.map(createEntry)}</div>
+			<h1>
+				<span>emojipedia</span>
+			</h1>
+			<div id="dictionary">
+				{emojipedia.map((emojidetails) => (
+					<Entry
+						key={emojidetails.id}
+						emoji={emojidetails.emoji}
+						name={emojidetails.name}
+						meaning={emojidetails.meaning}
+					/>
+				))}
+			</div>
 		</div>
 	);
 }
